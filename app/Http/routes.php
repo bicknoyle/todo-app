9b1acc35d854d89cb5b9aa689a39d13ba9d65b18
vagrant <nsboyle@gmail.com>
2015-04-28 18:29:18 +0000

    Welcome page has navbar now, change homepage to the TaskController@index

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -13,7 +13,7 @@
 
 Route::get('/', 'WelcomeController@index');
 
-Route::get('home', 'HomeController@index');
+Route::get('home', 'TaskController@index');
 
 Route::model('tasks', 'App\Task');
 Route::resource('tasks', 'TaskController');
